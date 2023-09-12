210b59e74106da0198106324e92a5ff40459c49d
3229065958 <cdmanrique58@misena.edu.co>
2023-09-12 15:10:30 -0500

    subir docu,entos

--- CRUD relacional/EliminarDatos.php
+++ CRUD relacional/EliminarDatos.php
@@ -1,12 +1,12 @@
 <?php
-      include 'Conexion.php';
+      include 'db.php';
 
-    $Id = $_REQUEST['Id'];
-    $sql = "DELETE FROM productos WHERE IdProducto ='$Id'";
+    $Id = $_REQUEST['id'];
+    $sql = "DELETE FROM documento WHERE iddocumento ='$id'";
 
     $query = mysqli_query($conn,$sql);
     if ($query === TRUE) {
-        header("Location:Index.php");
+        header("Location:index.php");
     }
 
 ?>
---
+++ subir documento/includes/EliminarDatos.php
@@ -0,0 +1,12 @@
+
+<?php
+      include 'db.php';
+
+    $Id = $_REQUEST['id'];
+    $sql = "DELETE FROM documento WHERE id='$Id'";
+
+    $resultado = mysqli_query($conexion, $sql);
+    if ($resultado === TRUE) {
+    }
+
+?>
